10043098eebe4477130c482ab597a2c13d76c482
Medhansh Hinduja <93686318+Medhansh-Hinduja@users.noreply.github.com>
2022-05-25 18:29:07 -0400

    Time: 12 ms (35.97%), Space: 48.2 MB (11.86%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.java
+++ 12-integer-to-roman/12-integer-to-roman.java
@@ -10,120 +10,110 @@ class Solution {
         return s;
     }
     
-    // returns a 2 element array with the symbol to be concatenated to the string (if applicable) and
+    // returns a 2 element array with the symbol to be concatenated to the string and
     // the remaining number to be converted.
     public String[] toAdd(int n) {
-        String A[] = {"", ""};
-        String sym[] = {"I", "V", "X", "L", "C", "D", "M"};
-        int val[] = {1, 5, 10, 50, 100, 500, 1000, 4000};
-        if (n==4) {
-            A[0] = "IV";
-            A[1] = "0"; 
-        }
-        else if (n==9) {
-            A[0] = "IX";
-            A[1] = "0"; 
-        }
-        else if (n>=40 && n<50) {
-            A[0] = "XL";
-            A[1] = Integer.toString(n-40); 
-        }
-        else if (n>=90 && n<100) {
-            A[0] = "XC";
-            A[1] = Integer.toString(n-90); 
-        }
-        else if (n>=400 && n<500) {
-            A[0] = "CD";
-            A[1] = Integer.toString(n-400); 
-        }
-        else if (n>=900 && n<1000) {
-            A[0] = "CM";
-            A[1] = Integer.toString(n-900); 
-        }
-        else {
-            System.out.println("Here: " + n);
-            for(int a=0; a<val.length-1; a++) {
-                if(n >= val[a] && n < val[a+1]) {
-                    A[0] = sym[a];
-                    A[1] = Integer.toString(n-val[a]);
-                    break;
-                }
+        String A[] = {"",""};
+        if (n>=1 && n<5) {
+            if(n==4) {
+               A[0] = "IV";
+               A[1] = "0"; 
+            } 
+            else {
+                A[0] = "I";
+                A[1] = Integer.toString(n-1);
             }
+        } else if (n>=5 && n<10) {
+            if(n==9) {
+                A[0] = "IX";
+                A[1] = "0"; 
+            } 
+            else {
+                A[0] = "V";
+                A[1] = Integer.toString(n-5);
+            }
+        } else if (n>=10 && n<50) {
+            if(n>=40) {
+                A[0] = "XL";
+                A[1] = Integer.toString(n-40); 
+            } 
+            else {
+                A[0] = "X";
+                A[1] = Integer.toString(n-10);
+            }
+        } else if (n>=50 && n<100) {
+            if(n>=90) {
+                A[0] = "XC";
+                A[1] = Integer.toString(n-90); 
+            } 
+            else {
+                A[0] = "L";
+                A[1] = Integer.toString(n-50);
+            }
+        } else if (n>=100 && n<500) {
+            if(n>=400) {
+                A[0] = "CD";
+                A[1] = Integer.toString(n-400); 
+            } 
+            else {
+                A[0] = "C";
+                A[1] = Integer.toString(n-100);
+            }
+        } else if (n>=500 && n<1000) {
+            if(n>=900) {
+                A[0] = "CM";
+                A[1] = Integer.toString(n-900); 
+            } 
+            else {
+                A[0] = "D";
+                A[1] = Integer.toString(n-500);
+            }
+        } else {
+            A[0] = "M";
+            A[1] = Integer.toString(n-1000);
         }
         return A;
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    // // returns a 2 element array with the symbol to be concatenated to the string and
+    // // returns a 2 element array with the symbol to be concatenated to the string (if applicable) and
     // // the remaining number to be converted.
     // public String[] toAdd(int n) {
-    //     String A[] = {"",""};
-    //     if (n>=1 && n<5) {
-    //         if(n==4) {
-    //            A[0] = "IV";
-    //            A[1] = "0"; 
-    //         } 
-    //         else {
-    //             A[0] = "I";
-    //             A[1] = Integer.toString(n-1);
+    //     String A[] = {"", ""};
+    //     String sym[] = {"I", "V", "X", "L", "C", "D", "M"};
+    //     int val[] = {1, 5, 10, 50, 100, 500, 1000, 4000};
+    //     if (n==4) {
+    //         A[0] = "IV";
+    //         A[1] = "0"; 
+    //     }
+    //     else if (n==9) {
+    //         A[0] = "IX";
+    //         A[1] = "0"; 
+    //     }
+    //     else if (n>=40 && n<50) {
+    //         A[0] = "XL";
+    //         A[1] = Integer.toString(n-40); 
+    //     }
+    //     else if (n>=90 && n<100) {
+    //         A[0] = "XC";
+    //         A[1] = Integer.toString(n-90); 
+    //     }
+    //     else if (n>=400 && n<500) {
+    //         A[0] = "CD";
+    //         A[1] = Integer.toString(n-400); 
+    //     }
+    //     else if (n>=900 && n<1000) {
+    //         A[0] = "CM";
+    //         A[1] = Integer.toString(n-900); 
+    //     }
+    //     else {
+    //         System.out.println("Here: " + n);
+    //         for(int a=0; a<val.length-1; a++) {
+    //             if(n >= val[a] && n < val[a+1]) {
+    //                 A[0] = sym[a];
+    //                 A[1] = Integer.toString(n-val[a]);
+    //                 break;
+    //             }
     //         }
-    //     } else if (n>=5 && n<10) {
-    //         if(n==9) {
-    //             A[0] = "IX";
-    //             A[1] = "0"; 
-    //         } 
-    //         else {
-    //             A[0] = "V";
-    //             A[1] = Integer.toString(n-5);
-    //         }
-    //     } else if (n>=10 && n<50) {
-    //         if(n>=40) {
-    //             A[0] = "XL";
-    //             A[1] = Integer.toString(n-40); 
-    //         } 
-    //         else {
-    //             A[0] = "X";
-    //             A[1] = Integer.toString(n-10);
-    //         }
-    //     } else if (n>=50 && n<100) {
-    //         if(n>=90) {
-    //             A[0] = "XC";
-    //             A[1] = Integer.toString(n-90); 
-    //         } 
-    //         else {
-    //             A[0] = "L";
-    //             A[1] = Integer.toString(n-50);
-    //         }
-    //     } else if (n>=100 && n<500) {
-    //         if(n>=400) {
-    //             A[0] = "CD";
-    //             A[1] = Integer.toString(n-400); 
-    //         } 
-    //         else {
-    //             A[0] = "C";
-    //             A[1] = Integer.toString(n-100);
-    //         }
-    //     } else if (n>=500 && n<1000) {
-    //         if(n>=900) {
-    //             A[0] = "CM";
-    //             A[1] = Integer.toString(n-900); 
-    //         } 
-    //         else {
-    //             A[0] = "D";
-    //             A[1] = Integer.toString(n-500);
-    //         }
-    //     } else {
-    //         A[0] = "M";
-    //         A[1] = Integer.toString(n-1000);
     //     }
     //     return A;
     // }
