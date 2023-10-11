b825d32d62735ef2d6a20c45fa6c0bec593f28bf
Medhansh Hinduja <mhinduja@uwaterloo.ca>
2023-10-11 18:09:54 -0400

    Time: 119 ms (62.41%), Space: 17.5 MB (15.99%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -3,7 +3,9 @@ class Solution:
         n = len(nums) - 1
         m = len(nums)
         i = 0
-        j = 0
+        j = 1
+        k = 0
+        r = nums[0]
         max_stop = -1
         stops = [0]
         if (m == 1):
@@ -12,13 +14,15 @@ class Solution:
             maximum = -1
             if (nums[i] >= n):
                 break
-            j = i + 1
-            for j in range(i + nums[i] + 1) :
-                if (((j - i) + nums[j]) >= maximum):
-                    maximum = (j - i) + nums[j]
-                    max_stop = j
+            for k in range(j, j + r) :
+                if (((k - i) + nums[k]) >= maximum):
+                    maximum = (k - i) + nums[k]
+                    max_stop = k
             n -= (max_stop-i)        
             stops.append(max_stop)
             i = max_stop
+            j = k
+            r = nums[max_stop] - (j - max_stop - 1)
         
+        print(stops)
         return len(stops)
